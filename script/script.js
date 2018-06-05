d09bb118505a4f07a8c3f6e950dfe493e36a443b
Yash Mahalwal <yashmahalwal@gmail.com>
2018-06-05 11:31:35 +0530

    Added overlay

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// Set scroll to top
+// Set scroll to top when page is reloaded
 $(window).on('beforeunload', function () {
     $(window).scrollTop(0);
 });
@@ -114,128 +114,175 @@ particlesJS("particles-js", {
     },
     "retina_detect": true
 });
+//jQuery for all the other things
+$(document).ready(function () {
+    //Sticky dividers
+    function scrollCheck(fixedElement, parent) {
+        if (window.matchMedia('(min-width: 768px)').matches) {
 
-//Sticky dividers
-function scrollCheck(fixedElement, parent) {
-    if (window.matchMedia('(min-width: 769px)').matches) {
+            var correction = parseInt($("#main-nav").css("height"));
+            var topLimit = parent.position().top - correction;
+            var bottomLimit = topLimit + parseInt(parent.css("height")) - parseInt(fixedElement.css("height"));
 
-        var correction = parseInt($("#main-nav").css("height"));
-        var topLimit = parent.position().top - correction;
-        var bottomLimit = topLimit + parseInt(parent.css("height")) - parseInt(fixedElement.css("height"));
-
-        var check = $(this).scrollTop();
-        if (check < topLimit) {
-            if (!fixedElement.hasClass("at-top")) {
-                fixedElement.addClass("at-top");
-            }
-            if (fixedElement.hasClass("fixed-top")) {
-                fixedElement.removeClass("fixed-top");
-            }
-            if (fixedElement.hasClass("at-bottom")) {
-                fixedElement.removeClass("at-bottom");
-            }
-        } else if (check >= topLimit && check <= bottomLimit) {
-            if (fixedElement.hasClass("at-top")) {
-                fixedElement.removeClass("at-top");
-            }
-            if (!fixedElement.hasClass("fixed-top")) {
-                fixedElement.addClass("fixed-top");
-            }
-            if (fixedElement.hasClass("at-bottom")) {
-                fixedElement.removeClass("at-bottom");
-            }
-        } else if (check > bottomLimit) {
-            if (fixedElement.hasClass("at-top")) {
-                fixedElement.removeClass("at-top");
-            }
-            if (fixedElement.hasClass("fixed-top")) {
-                fixedElement.removeClass("fixed-top");
-            }
-            if (!fixedElement.hasClass("at-bottom")) {
-                fixedElement.addClass("at-bottom");
+            var check = $(this).scrollTop();
+            if (check < topLimit) {
+                if (!fixedElement.hasClass("at-top")) {
+                    fixedElement.addClass("at-top");
+                }
+                if (fixedElement.hasClass("fixed-top")) {
+                    fixedElement.removeClass("fixed-top");
+                }
+                if (fixedElement.hasClass("at-bottom")) {
+                    fixedElement.removeClass("at-bottom");
+                }
+            } else if (check >= topLimit && check <= bottomLimit) {
+                if (fixedElement.hasClass("at-top")) {
+                    fixedElement.removeClass("at-top");
+                }
+                if (!fixedElement.hasClass("fixed-top")) {
+                    fixedElement.addClass("fixed-top");
+                }
+                if (fixedElement.hasClass("at-bottom")) {
+                    fixedElement.removeClass("at-bottom");
+                }
+            } else if (check > bottomLimit) {
+                if (fixedElement.hasClass("at-top")) {
+                    fixedElement.removeClass("at-top");
+                }
+                if (fixedElement.hasClass("fixed-top")) {
+                    fixedElement.removeClass("fixed-top");
+                }
+                if (!fixedElement.hasClass("at-bottom")) {
+                    fixedElement.addClass("at-bottom");
+                }
             }
         }
     }
-}
 
-function scrollCheckOdd(fixedElement, parent) {
-    if (window.matchMedia('(min-width: 769px)').matches) {
+    function scrollCheckOdd(fixedElement, parent) {
+        if (window.matchMedia('(min-width: 768px)').matches) {
 
-        var correction = parseInt($("#main-nav").css("height"));
-        var topLimit = parent.position().top - correction;
-        var bottomLimit = topLimit + parseInt(parent.css("height")) - parseInt(fixedElement.css("height"));
+            var correction = parseInt($("#main-nav").css("height"));
+            var topLimit = parent.position().top - correction;
+            var bottomLimit = topLimit + parseInt(parent.css("height")) - parseInt(fixedElement.css("height"));
 
-        var check = $(this).scrollTop();
-        if (check < topLimit) {
-            if (!fixedElement.hasClass("at-top-odd")) {
-                fixedElement.addClass("at-top-odd");
-            }
-            if (fixedElement.hasClass("fixed-top-odd")) {
-                fixedElement.removeClass("fixed-top-odd");
-            }
-            if (fixedElement.hasClass("at-bottom-odd")) {
-                fixedElement.removeClass("at-bottom-odd");
-            }
-        } else if (check >= topLimit && check <= bottomLimit) {
-            if (fixedElement.hasClass("at-top-odd")) {
-                fixedElement.removeClass("at-top-odd");
-            }
-            if (!fixedElement.hasClass("fixed-top-odd")) {
-                fixedElement.addClass("fixed-top-odd");
-            }
-            if (fixedElement.hasClass("at-bottom-odd")) {
-                fixedElement.removeClass("at-bottom-odd");
-            }
-        } else if (check > bottomLimit) {
-            if (fixedElement.hasClass("at-top-odd")) {
-                fixedElement.removeClass("at-top-odd");
-            }
-            if (fixedElement.hasClass("fixed-top-odd")) {
-                fixedElement.removeClass("fixed-top-odd");
-            }
-            if (!fixedElement.hasClass("at-bottom-odd")) {
-                fixedElement.addClass("at-bottom-odd");
+            var check = $(this).scrollTop();
+            if (check < topLimit) {
+                if (!fixedElement.hasClass("at-top-odd")) {
+                    fixedElement.addClass("at-top-odd");
+                }
+                if (fixedElement.hasClass("fixed-top-odd")) {
+                    fixedElement.removeClass("fixed-top-odd");
+                }
+                if (fixedElement.hasClass("at-bottom-odd")) {
+                    fixedElement.removeClass("at-bottom-odd");
+                }
+            } else if (check >= topLimit && check <= bottomLimit) {
+                if (fixedElement.hasClass("at-top-odd")) {
+                    fixedElement.removeClass("at-top-odd");
+                }
+                if (!fixedElement.hasClass("fixed-top-odd")) {
+                    fixedElement.addClass("fixed-top-odd");
+                }
+                if (fixedElement.hasClass("at-bottom-odd")) {
+                    fixedElement.removeClass("at-bottom-odd");
+                }
+            } else if (check > bottomLimit) {
+                if (fixedElement.hasClass("at-top-odd")) {
+                    fixedElement.removeClass("at-top-odd");
+                }
+                if (fixedElement.hasClass("fixed-top-odd")) {
+                    fixedElement.removeClass("fixed-top-odd");
+                }
+                if (!fixedElement.hasClass("at-bottom-odd")) {
+                    fixedElement.addClass("at-bottom-odd");
+                }
             }
         }
     }
-}
-$(window).scroll(function () {
-    scrollCheck($("#first-a"), $("#first-container"));
-    scrollCheck($("#third-a"), $("#third-container"));
-
-    scrollCheckOdd($("#second-a"), $("#second-container"));
-    var heroLimit = $("#hero-top").position().top + parseInt($("#hero-top").css("height"));
-    heroLimit -= parseInt($("#main-nav").css("height")) + 50;
-    if ($(window).scrollTop() > heroLimit) {
-        $("#main-nav").css({
-            "visibility": "visible",
-        });
-    } else {
-        $("#main-nav").css({
-            "visibility": "hidden",
-        });
+    // Function to close the main nav menu
+    function navClose() {
+        var menu = $("#main-nav").find(".navbar-menu");
+        if (menu.hasClass("is-active")) {
+            menu.removeClass("is-active");
+        }
+    }
+    // Function to open the main navbar menu
+    function navOpen() {
+        $("#navbar-overlay").toggleClass(".overlay-active", true);
     }
 
-});
-$(window).resize(function () {
-    scrollCheck($("#first-a"), $("#first-container"));
-    scrollCheck($("#third-a"), $("#third-container"));
-    scrollCheckOdd($("#second-a"), $("#second-container"));
+    $(window).scroll(function () {
+        scrollCheck($("#first-a"), $("#first-container"));
+        scrollCheck($("#third-a"), $("#third-container"));
 
-    if ($(".navbar-burger").hasClass("active")) {
-        $(".navbar-burger").removeClass("active");
-    }
+        scrollCheckOdd($("#second-a"), $("#second-container"));
+        // Making the navbar come and go.
+        var heroLimit = $("#hero-top").position().top + parseInt($("#hero-top").css("height"));
+        heroLimit -= parseInt($("#main-nav").css("height")) + 30;
+        if ($(window).scrollTop() > heroLimit) {
+            $("#main-nav").css({
+                "visibility": "visible",
+                "opacity": "1",
+                "transition": "700ms"
+            });
+        } else {
+            $("#main-nav").css({
+                "visibility": "hidden",
+                "opacity": "0",
+                "transition": "700ms"
+            });
+        }
+
+    });
+    $(window).resize(function () {
+        scrollCheck($("#first-a"), $("#first-container"));
+        scrollCheck($("#third-a"), $("#third-container"));
+        scrollCheckOdd($("#second-a"), $("#second-container"));
+    });
 });
+
+$("#navbar-overlay").click(function () {
+    $("#main-nav").find(".navbar-menu").removeClass("is-active");
+    $(this).css({
+        "visibility": "hidden",
+        "opacity": "0",
+        "transition": "700ms"
+    });
+    $("#main-nav").find(".navbar-burger").toggleClass("is-active");
+});
+
+
 
 $(".navbar-burger").click(function () {
     $(this).toggleClass("is-active");
+
+
+    var navbar = $(this).parents(".navbar");
+    if (!navbar.find(".navbar-menu").hasClass("is-active")) {
+        navbar.find(".navbar-menu").addClass("is-active");
+        $("#navbar-overlay").css({
+            "visibility": "visible",
+            "opacity": "0.3",
+            "transition": "700ms"
+        });
+    } else if (navbar.find(".navbar-menu").hasClass("is-active")) {
+        navbar.find(".navbar-menu").removeClass("is-active");
+        $("#navbar-overlay").css({
+            "visibility": "hidden",
+            "opacity": "0",
+            "transition": "700ms"
+        });
+    }
 });
 
 $(".my-card-holder").click(function () {
+
     $("#third-a").addClass("divider-half");
     $(".my-card-holder").each(function () {
         $(this).addClass("card-disappear");
     });
+
     $("#third-b").css({
         "width": "80vw",
         "margin-left": "20vw",
@@ -246,5 +293,5 @@ $(".my-card-holder").click(function () {
     });
     $('html, body').animate({
         scrollTop: $("#third-b").offset().top - parseInt($("#main-nav").css("height"))
-    }, 2000);
+    }, 500);
 });
